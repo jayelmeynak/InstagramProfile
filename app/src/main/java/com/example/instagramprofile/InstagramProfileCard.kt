301fb9282baac12461bb8b41050127451a2950a9
jayelmeynak <magomed110301@yandex.ru>
2024-07-09 01:19:53 +0300

    Implemented FollowButton

--- app/src/main/java/com/example/instagramprofile/InstagramProfileCard.kt
+++ app/src/main/java/com/example/instagramprofile/InstagramProfileCard.kt
@@ -14,11 +14,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,7 +33,8 @@ import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun InstagramProfileCard(topPadding: PaddingValues) {
+fun InstagramProfileCard(topPadding: PaddingValues, viewModel: MainViewModel) {
+    val isFollowed = viewModel.isFollowing.observeAsState(initial = true)
     Card(
         modifier = Modifier.padding(top = topPadding.calculateTopPadding()),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
@@ -75,19 +78,38 @@ fun InstagramProfileCard(topPadding: PaddingValues) {
                 fontSize = 14.sp,
                 modifier = Modifier.padding(4.dp)
             )
-            Button(
-                onClick = {},
-                modifier = Modifier.padding(4.dp)
-            ) {
-                Text(
-                    text = "Follow",
-                    fontSize = 20.sp
-                )
-
+            FollowButton(isFollowed = isFollowed.value) {
+                viewModel.changeFollowingState()
             }
         }
     }
 
+}
+
+
+@Composable
+private fun FollowButton(
+    isFollowed: Boolean,
+    clickListener: () -> Unit
+) {
+    Button(
+        onClick = { clickListener() },
+        modifier = Modifier.padding(4.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isFollowed) {
+                MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            } else MaterialTheme.colorScheme.primary
+        )
+    ) {
+        val text = if (isFollowed) {
+            "Unfollowed"
+        } else "Followed"
+        Text(
+            text = text,
+            fontSize = 20.sp
+        )
+
+    }
 }
 
 @Composable
--- app/src/main/java/com/example/instagramprofile/MainActivity.kt
+++ app/src/main/java/com/example/instagramprofile/MainActivity.kt
@@ -7,16 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
 import com.example.instagramprofile.ui.theme.InstagramProfileTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         enableEdgeToEdge()
         setContent {
             InstagramProfileTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { topPadding ->
-                    InstagramProfileCard(topPadding)
+                    InstagramProfileCard(topPadding, viewModel)
                 }
             }
         }
